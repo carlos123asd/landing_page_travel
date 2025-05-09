6bb67b79f93e565c0ea1e294f1654d9b97a6220e
carlos123asd <carlos-medin@hotmail.com>
2025-05-09 18:40:38 +0200

    FIX: menu size

--- src/component/atom/Menu.tsx
+++ src/component/atom/Menu.tsx
@@ -1,11 +1,24 @@
 export default function Menu(){
     const menu = ["machu picchu","london","singapore","bali"];
+
+    const getWidthWord = (letters:number) => {
+        if(letters === 4){
+            return '200'
+        }else  if(letters === 6){
+            return '330'
+        }else  if(letters === 9){
+            return '450'
+        }else  if(letters >= 11){
+            return '600'
+        }
+    }
+
     return <>
         <div className="menu">
             {menu.map((titulo:string,index:number) => {
                 index = index + 1;
-                return <div className="menuItem">
-                        <svg width="100%" height="100" viewBox="0 0 650 100">
+                return <div key={index} className="menuItem">
+                        <svg width="100%" height="100" viewBox={`0 0 ${getWidthWord(titulo.length)} 100`}>
                             <text
                                 className="titMenu"
                                 x="0"
@@ -13,7 +26,7 @@ export default function Menu(){
                                 fontWeight="bold"
                                 fill="transparent"
                                 stroke="white"
-                                strokeWidth="0.01"
+                                strokeWidth=".01"
                             >
                                 {titulo}
                             </text>
